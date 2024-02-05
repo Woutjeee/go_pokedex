13fbb152bec75466c9d2ad391f3f96b081be75b0
Woutjeee <wout.hiemstra@hotmail.com>
2024-02-05 18:21:04 +0100

    main loop working

--- cmd/go_pokedex/main.go
+++ cmd/go_pokedex/main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/Woutjeee/go_pokedex/internal/commands"
 )
+
+var cliName string = "Pokedex"
+
+func printPrompt() {
+	fmt.Print(cliName, " > ")
+}
+
+func cleanInput(text string) string {
+	output := strings.TrimSpace(text)
+	output = strings.ToLower(output)
+	return output
+}
 
 func main() {
 	fmt.Println("Hello, World!")
 
 	commands := commands.GetCommands()
 
-	fmt.Println(commands)
+	reader := bufio.NewScanner(os.Stdin)
+	printPrompt()
+	for reader.Scan() {
+		text := cleanInput(reader.Text())
+		if command, exists := commands[text]; exists {
+			fmt.Println(command)
+		} else {
+			fmt.Println("Does not exist")
+		}
+		printPrompt()
+	}
 }
